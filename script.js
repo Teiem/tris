902a8cc6e33a544205b2c8a7326016057c1026b2
Tim <teiem@yahoo.com>
2019-11-29 00:22:23 +0100

    Added advanced selection

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const main = document.getElementById("main");
 
 const tris = (() => {
-    const globalState = {
+    const state = {
         arr: null,
-        selection: null,
-        disableChanges: false,
+        selectionArr: null,
+        selectCallback: null,
         colorMain: "black",
         colorSec: "white",
         colorMix: "grey",
+        mouseButton: null,
+        drag: {
+            wasSelected: false,
+            mouseHasBeenMoved: false,
+            init: [null, null],
+            last: {
+                start: null,
+                end: null
+            },
+            curEl: null,
+            lastEl: null,
+            cur: [null, null],
+        }
     };
 
     const changeState = {
@@ -16,61 +29,67 @@ const tris = (() => {
         is: []
     };
 
-    // y and x are swaped
-    const change = (x, y, index, e) => {
-        if (globalState.disableChanges || (changeState.x !== null && (changeState.x !== x)) || (changeState.y !== null && (changeState.y !== y)) || changeState.is.includes(index)) return;
+    const unselectTris = (el = get([changeState.x, changeState.y])) => {
+        el.parentElement.classList.remove("focus");
+        main.classList.remove("focused");
+        [...el.parentElement.children].forEach(el => {
+            el.classList.remove("activated");
+            el.style.willChange = "initial";
+        });
 
-        changeState.x = changeState.x || x;
-        changeState.y = changeState.y || y;
-        e.target.parentElement.classList.add("focus")
-        e.target.classList.add("activated");
+        /* makes Tris clickable again */
+        setTimeout(() => {
+            changeState.x = null;
+            changeState.y = null;
+            changeState.is = [];
+        }, 200);
+    };
+    const change = ([x, y], index, el) => {
+        if ((changeState.x !== null && (changeState.x !== y)) || (changeState.y !== null && (changeState.y !== x)) || changeState.is.includes(index)) return;
+
+        changeState.x = changeState.x || y;
+        changeState.y = changeState.y || x;
+        el.parentElement.classList.add("focus")
+        el.classList.add("activated");
         main.classList.add("focused");
-
 
         changeState.is.push(index);
 
         if (changeState.is.length !== 3) return;
         [0, 1, 2, 3].some(i => !changeState.is.includes(i) && changeState.is.push(i))
 
-        globalState.arr[x][y] = changeState.is;
-        [...get(y, x)].forEach((el, i) => {
-            el.firstElementChild.style.transform = `rotate(${changeState.is.findIndex(it => it === i) * 90}deg)`;
+        //change style back
+        state.arr[y][x] = changeState.is;
+        [...get([x, y]).children].forEach((el, i) => {
             el.style.willChange = "rotate";
+            el.firstElementChild.style.transform = `rotate(${changeState.is.findIndex(it => it === i) * 90}deg)`;
         })
 
-        setTimeout(() => {
-            e.target.parentElement.classList.remove("focus");
-            main.classList.remove("focused");
-            [...e.target.parentElement.children].forEach(el => {
-                // console.log(el.style);
-
-                el.classList.remove("activated");
-                el.style.willChange = "initial";
-            });
-        }, 200);
-
-        setTimeout(() => {
-            changeState.x = null;
-            changeState.y = null;
-            changeState.is = [];
-        }, 400);
+        setTimeout(() => unselectTris(el), 200);
     };
 
-    const get = (x, y) => main.children[y * globalState.arr[0].length + x].children
+    /**
+     * 
+     * @param {[number, number]} param0 
+     */
+    const get = ([x, y]) => main.children[getIndex([x, y])];
+    const getIndex = ([x, y]) => y * state.arr[0].length + x;
+    const getXY = index => [index % state.arr[0].length, Math.floor(index / state.arr[0].length)];
+    const getXYFromEl = el => [+el.getAttribute("data-x"), +el.getAttribute("data-y")];
 
-    const getTriangle = (x, y, i, rot) => `    
-    <svg width="100" height="100" viewBox="0 0 100 100" onclick="tris.clickHandler(${x}, ${y}, ${i}, event)">
+    const getTriangle = ([x, y], i, rot) => `    
+    <svg width="100" height="100" viewBox="0 0 100 100" onmousedown="tris.mouseDownHandler([${x}, ${y}], ${i}, event)">
         <polygon points="0,0 0,100 100,100" style="fill: var(--colorMain); transform: rotate(${rot * 90}deg); pointer-events: none;"/>
     </svg>`;
 
     const generate = () => {
         applyColor();
         let htmlString = "";
-        globalState.arr.forEach((subarr, y) => {
+        state.arr.forEach((subarr, y) => {
             subarr.forEach((el, x) => {
                 htmlString += `
                 <div class="group" data-x="${x}" data-y="${y}" style="${x === 0 ? "clear:left" : ""}">
-                    ${el.map((rot, i) => getTriangle(y, x, i, rot)).join("")}
+                    ${el.map((rot, i) => getTriangle([x, y], i, rot)).join("")}
                 </div>
                 `;
             })
@@ -91,109 +110,268 @@ const tris = (() => {
 
         if (colorMain) setColorMain(colorMain);
         if (colorSec) setColorSec(colorSec);
-        globalState.arr = (new Array(h)).fill("").map(_ => (new Array(w).fill(shape)));
-        globalState.selection = (new Array(h)).fill("").map(_ => (new Array(w).fill(false)));
+        state.arr = (new Array(h)).fill("").map(_ => (new Array(w).fill(shape)));
+        state.selectionArr = (new Array(h)).fill("").map(_ => (new Array(w).fill(false)));
         generate()
     };
 
     // Colors
     const setColorMain = (color) => {
-        globalState.colorMain = color
+        state.colorMain = color
     };
 
     const setColorSec = (color) => {
-        globalState.colorSec = color
+        state.colorSec = color
     };
 
     const applyColor = () => {
-        document.documentElement.style.setProperty("--colorMain", globalState.colorMain);
-        document.documentElement.style.setProperty("--colorSec", globalState.colorSec);
-        document.documentElement.style.setProperty("--colorMix", globalState.colorMix);
+        document.documentElement.style.setProperty("--colorMain", state.colorMain);
+        document.documentElement.style.setProperty("--colorSec", state.colorSec);
+        document.documentElement.style.setProperty("--colorMix", state.colorMix);
     };
 
-    const swapColors = () => ([globalState.colorMain, globalState.colorSec] = [globalState.colorSec, globalState.colorMain]) && applyColor();
+    const swapColors = () => ([state.colorMain, state.colorSec] = [state.colorSec, state.colorMain]) && applyColor();
 
-    let selectedElements = [];
+    /**
+     * updates visual representation of newly selected elements
+     */
+    const animate = () => {
+        if (state.drag.curEl === state.drag.lastEl) return;
 
-    const clickHandler = (x, y, i, e) => {
-        console.log("click", e);
-        if (e.ctrlKey) {
-            const _index = selectedElements.indexOf(e.target.parentElement);
-            return ~_index ? functs.removeFromSelection(x, y, _index) : functs.addToSelection(x, y, e.target.parentElement);
+        state.drag.lastEl = state.drag.curEl;
+        state.drag.mouseHasBeenMoved = true;
+
+        const curCoords = getXYFromEl(state.drag.curEl);
+        
+        if (state.mouseButton === 2) {
+            const curStart = [Math.min(state.drag.init[0], curCoords[0]), Math.min(state.drag.init[1], curCoords[1])];
+            const curEnd = [Math.max(state.drag.init[0], curCoords[0]), Math.max(state.drag.init[1], curCoords[1])];
+    
+            const startCords = [Math.min(curStart[0], state.drag.last.start[0]), Math.min(curStart[1], state.drag.last.start[1])];
+            const endCords = [Math.max(curEnd[0], state.drag.last.end[0]), Math.max(curEnd[1], state.drag.last.end[1])];
+    
+            for (let y = startCords[1]; y <= endCords[1]; y++) {
+                for (let x = startCords[0]; x <= endCords[0]; x++) {
+                    
+                    const isInCur = x >= curStart[0] && y >= curStart[1] && x <= curEnd[0] && y <= curEnd[1];
+                    const isInLast = x >= state.drag.last.start[0] && y >= state.drag.last.start[1] && x <= state.drag.last.end[0] && y <= state.drag.last.end[1];
+    
+                    if (isInCur && isInLast) continue;
+                    (isInCur && !isInLast) === state.drag.wasSelected ? functs.removeFromSelection([x, y], get([x, y])) : functs.addToSelection([x, y], get([x, y]))
+                }
+            }
+    
+            state.drag.last.start = curStart;
+            state.drag.last.end = curEnd;
+        } else if (state.mouseButton === 0) {
+            if (!state.drag.addedFirst) {
+                state.drag.wasSelected ? functs.removeFromSelection(state.drag.init, state.drag.initEl) : functs.addToSelection(state.drag.init, state.drag.initEl)
+                state.drag.addedFirst = true;
+            }
+            state.drag.wasSelected ? functs.removeFromSelection(curCoords, state.drag.curEl) : functs.addToSelection(curCoords, state.drag.curEl)
         }
-        change(x, y, i, e)
     };
+
+    /**
+     * Initial function for Mouse interaction, gets called from inline onlick event
+     * @param {number} y 
+     * @param {number} x 
+     * @param {number} i index of all tiles
+     * @param {Event} e event
+     */
+    const mouseDownHandler = ([x, y], i, e) => {
+        if (changeState.x !== null) return change([x, y], i, e.target);
+
+        console.log(e);
+        
+        state.drag.init = [x, y];
+        state.drag.initEl = e.target.parentElement;
+        state.drag.lastEl = e.target.parentElement;
+        state.drag.wasSelected = state.selectionArr[y][x];
+        state.drag.mouseHasBeenMoved = false;
+        state.drag.addedFirst = false;
+
+        state.drag.last.start = [x + 1, y + 1];
+        state.drag.last.end = [x - 1, y - 1];
+
+        state.mouseButton = e.button;
+
+        main.addEventListener("mouseover", mouseMoveHandler)
+        document.addEventListener("mouseup", () => mouseUpHandler([x, y], i, e), {
+            once: true
+        })
+    };
+
+    const mouseUpHandler = ([x, y], i, e) => {
+        main.removeEventListener("mouseover", mouseMoveHandler)
+
+        if (state.drag.mouseHasBeenMoved) return;
+
+        clickHandler(y, x, i, e);
+    };
+
+    const mouseMoveHandler = e => {
+        state.drag.curEl = e.target.parentElement;
+        requestAnimationFrame(animate);
+    };
+
+    const clickHandler = (y, x, i, e) => {
+        console.log("click", x, y);
+
+        const _el = e.target.parentElement;
+        if (state.selectCallback) {
+            const _cc = state.selectCallback;
+            state.selectCallback = null;
+            return _cc(_el, [x, y]);
+
+        }
+        if (e.ctrlKey) {
+            const _isSelected = state.selectionArr[y][x];
+            return _isSelected ? functs.removeFromSelection([x, y], _el) : functs.addToSelection([x, y], _el);
+
+        }
+        if (e.shiftKey) {
+            const areRemoving = state.selectionArr[y][x];
+            functs.addToSelection([x, y], _el);
+            return functs.selectOneOnNextClick(toEl => {
+                functs.callForMultiple([x, y], getXYFromEl(toEl), areRemoving ? functs.removeFromSelection : functs.addToSelection);
+            })
+
+        }
+        // only used first time;
+        change([x, y], i, e.target)
+    };
+
+    const keyBoardHandler = e => {
+        console.log("keyDown", e.keyCode, e);
+        
+        if (e.key === "a" && e.ctrlKey) {
+            functs.select()
+            e.preventDefault()
+        } else if (e.keyCode === 27) {
+            functs.unselect();
+            unselectTris();
+        }
+    };
+
+    document.addEventListener("keydown", keyBoardHandler);
+    main.addEventListener("contextmenu", e => e.preventDefault());
+
 
     const functs = (() => {
-        //css to give mu,tiple elements one border
+        //could be done better, pseudo element below tile with border - problem shadow
 
-        //swap x,y
-        const addToSelection = (x, y, el) => {
+        const updateBorder = ([x, y]) => {
+            get([x, y]).style.clipPath = "inset(" + [
+                [x, y - 1],
+                [x + 1, y],
+                [x, y + 1],
+                [x - 1, y],
+            ].map(([x, y]) => state.selectionArr[y] !== undefined && state.selectionArr[y][x] ? 4 : -40).join("px ") + "px)"
+        };
+
+        const addToSelection = ([x, y], el) => {
+            if (state.selectionArr[y][x]) return;
+
             el.classList.add("selected");
-            selectedElements.push(el);
 
-            const _t = globalState.selection;
-            console.log(x, y, _t);
-            
-            _t[x][y] = true;
+            const _t = state.selectionArr;
+
+            _t[y][x] = true;
             [
                 [x + 1, y],
                 [x - 1, y],
                 [x, y + 1],
                 [x, y - 1],
-            ].forEach(([x, y], side) => {;
-                
-                console.log([x, y], _t);
-                
-                _t[y] && _t[y][x] && (get(x, y).style["border" + ["Top", "Bottom", "Right", "Left"][side], + "Width"] = "0")
+            ].forEach(([x, y]) => {
+                if (_t[y] !== undefined && _t[y][x]) {
+                    updateBorder([x, y]);
+                }
             })
-
+            updateBorder([x, y]);
         };
 
-        const removeFromSelection = (x, y, index) => {
-            selectedElements[index].classList.remove("selected");
-            selectedElements.splice(index, 1);
+        const removeFromSelection = ([x, y], el) => {
+            if (!state.selectionArr[y][x]) return;
 
-            console.log(selectedElements);
-            
-        };
+            el.classList.remove("selected");
+            el.style.clipPath = "initial"
 
-        const selectOne = (callback) => {
-            tris.globalState.disableChanges = true;
-            main.addEventListener("click", (e) => {
-                const curEl = e.target.parentElement;
-                addToSelection(curEl);  
-                tris.globalState.disableChanges = false;
-                callback(curEl)
-            }, {
-                once: true
+            const _t = state.selectionArr;
+
+            _t[y][x] = false;
+            [
+                [x + 1, y],
+                [x - 1, y],
+                [x, y + 1],
+                [x, y - 1],
+            ].forEach(([x, y]) => {
+                if (_t[y] !== undefined && _t[y][x]) {
+                    updateBorder([x, y]);
+                }
             })
         };
 
-        const unselect = () => (selectedElements.forEach(el => el.classList.remove("selected"))) || (selectedElements = [])
+        const callForMultiple = ([fromX, fromY], [toX, toY], callback) => {
+            if (fromX === toX && fromY === toY) return addToSelection([fromX, fromY], get([fromX, fromY]));
 
-        const copy = e => selectOne(copyFrom => selectOne(copyTo => [...copyFrom.children].forEach((copyFromSVGs, i) => [...copyTo.children][i].firstElementChild.style.transform = copyFromSVGs.firstElementChild.style.transform) || unselect()));
-        const swap = e => selectOne(select1 => selectOne(select2 => [...select1.children].forEach((copyFromSVGs, i) => [...select2.children][i].firstElementChild.style.transform = copyFromSVGs.firstElementChild.style.transform) || unselect()));
+            const fromIndex = getIndex([fromX, fromY]);
+            const toIndex = getIndex([toX, toY]);
+            const dirIsForward = fromIndex < toIndex;
 
-        const change = () => {};
+            for (let i = fromIndex; dirIsForward ? i <= toIndex : i >= toIndex; dirIsForward ? i++ : i--) {
+                callback(getXY(i), get(getXY(i)));
+            }
+        };
 
-        const symTopDown = () => { // ???
-            arr = arr.map((el, index) => index < arr.length ? el : arr[arr.length - el])
-            tris.generate(arr);
+        const selectOneOnNextClick = callback => {
+            state.selectCallback = (el, [x, y]) => {
+                addToSelection([x, y], el)
+                callback(el, [x, y]);
+            };
+        };
+
+        const unselect = () => state.selectionArr.forEach((subArr, y) => subArr.forEach((el, x) => state.selectionArr[y][x] && removeFromSelection([x, y], get([x, y]))))
+
+        const select = () => state.selectionArr.forEach((subArr, y) => subArr.forEach((el, x) => !state.selectionArr[y][x] && addToSelection([x, y], get([x, y]))))
+
+        const copy = e => {
+            selectOneOnNextClick(copyFrom => {
+                selectOneOnNextClick(copyTo => {
+                    [...copyFrom.children].forEach((copyFromSVGs, i) => {
+                        [...copyTo.children][i].firstElementChild.style.transform = copyFromSVGs.firstElementChild.style.transform;
+                        unselect()
+                    })
+                })
+            })
+        }
+        const swap = e => selectOneOnNextClick(select1 => selectOneOnNextClick(select2 => [...select1.children].forEach((copyFromSVGs, i) => [...select2.children][i].firstElementChild.style.transform = copyFromSVGs.firstElementChild.style.transform) || unselect()));
+
+        const _symTopDown = () => { // ???
+            state.arr = state.arr.map(subArr => subArr.reverse());
+            tris.generate(state.arr);
         };
 
         return {
             addToSelection,
             removeFromSelection,
+            callForMultiple,
+            selectOneOnNextClick,
+            unselect,
+            select,
             copy,
+            _db: {
+                _symTopDown,
+            }
         };
     })();
 
     return {
-        globalState,
+        globalState: state,
         change,
         generate,
         create,
+        mouseDownHandler,
         clickHandler,
         functs,
         db: {
